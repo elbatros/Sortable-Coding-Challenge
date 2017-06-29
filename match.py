bef5e3d354ad59cbfea87b2208e15ddc06cb8d9f
Siddhant Gawsane <siddhantgawsane@gmail.com>
2017-06-29 06:12:39 -0500

    similarity check

--- match.py
+++ match.py
@@ -1,9 +1,18 @@
 import json
 from itertools import ifilter
 from difflib import SequenceMatcher
+from operator import itemgetter
 
 def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
+    # return SequenceMatcher(None, a, b).ratio()
+    score = 0.0
+    for word in b.split():
+    	if word in a:
+    		score = score + 1
+    score = score / len(a)
+    # print score
+    return score
+
 
 listings_file = open ('challenge_data_20110429/listings.txt')
 
@@ -22,17 +31,20 @@ for line in products_file:
 # print(products[0]['manufacturer'])
 
 
-for listing in listings:
+for listing in listings[:10]:
 	# iterator = ifilter(lambda x: x['manufacturer'] is listing["manufacturer"], products)
 	print listing
 	match = []
 	for product in products:
-		print product['manufacturer']
+		# print product['manufacturer']
 		score = 0
-		score = score + similar(listing['manufacturer'], product['manufacturer'])
-		print score
+		score = score + similar(listing['manufacturer'], product['manufacturer']) * 4
+		# print score
 		score = score + similar(listing['title'], product['manufacturer'])
-		print score
+		# print score
 		# 	print product
-		break
-	break
+		# break
+		# print match
+		match.append({'score':score, 'product': product})
+	print sorted(match, key=itemgetter('score'), reverse=True)[0]
+	# break
